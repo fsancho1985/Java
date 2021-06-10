6fdc4f0fa09fe30cf0334a8a11f1b2694d960fd8
Francisco <fr_sancho@icloud.com>
2021-06-10 18:24:38 -0300

    Update

--- Primeia.java
+++ Primeia.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 class Primeira {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Curso de Java");
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Hello, What's your name?");
+        String name = entrada.next();
+
+        System.out.println("Hi " + name + ", How are you?");
     }
 }
